d2cb71bd8d9cbd0cc60f85b04443a77b57212051
Ngarcia04 <123008098+Ngarcia04@users.noreply.github.com>
2023-12-03 20:53:59 -0500

    navigation

---
+++ nav.php
@@ -0,0 +1,5 @@
+<nav>
+    <a href="displayShelves.php"><button><img src="images/shelves_button.png"></button></a>
+    
+    <a href="profile.php"><button><img src="images/profile_pic.png"></button></a>
+</nav>
