b98910e5b241380783a9c3322791a820be147566
AdaliaWilliams <97926269+AdaliaWilliams@users.noreply.github.com>
2023-12-10 20:49:09 -0500

    Update addFriends.php

--- addFriends.php
+++ addFriends.php
@@ -1,24 +1,29 @@
 <?php
 //webpage for adding the user's friends
+include 'top.php';
 ?>
 <main>
     <section>
         <p class="back"><a href="friends.php">BACK</a></p>
     </section>
-    <section>
-        <form>
-            <fieldset>
+
+    <h2>Add A Friend</h2>
+    <form>
+        <fieldset>
+            <p>
+                <label>Username:<label>
+                        <input type="text">
+            </p>
+        </fieldset>
+        <fieldset>
                 <p>
-                    <label>Username:<label>
-                    <input type="text">
+                    <input type="submit" value="Add">
                 </p>
-            </fieldset>
-            <fieldset>
-                <p>
-                    <input type="submit">
-                </p>
-            </fieldset>
-        </form>
-    </section>
+        </fieldset>
+    </form>
+
+
 </main>
-</html>
+<?php
+include 'nav.php'
+?>
